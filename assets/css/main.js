bb72d1dd5b9e87f6e0df98b605584bfa9ed43bfa
S4VENGX <luunguyen3112005@gmail.com>
2025-03-02 16:57:03 +0700

    Uploaded 02/03/2025

---
+++ assets/css/main.js
@@ -0,0 +1,59 @@
+// alert("Welcome");
+// prompt("Enter something:");
+// document.write("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+// console.error("exxxxx");
+// console.warn("warning!!!!!!")
+
+/*---------------------------------------------------------*/
+
+// var name = prompt("Enter your name");
+// alert("Hello " + name);
+// var num1 = prompt("Enter num1");
+// var num2 = prompt("Enter num2");
+// alert(parseInt(num1) + parseInt(num2));
+
+// var a = prompt("Enter your number want to print");
+// for(var i = 0; i <= a; i++){
+//     document.writeln(i);
+// }
+
+/*---------------------------------------------------------*/
+
+// var name = prompt();
+// document.write("<h1>" + name + "</h1>");
+
+// var num = prompt();
+// var flag = true;
+
+// for(var i = 1; i <= num; i++){
+//     for(var j = 2; j < i; j++){
+//         if(i % j == 0){
+//             flag = false;
+//             break;
+//         }
+//     }
+//     if(flag == true){
+//         document.write(i + " ");
+//     }
+//     flag = true;
+// }
+
+/*---------------------------------------------------------*/
+
+// function displayDate(){
+//     document.getElementById('para1output').innerHTML = Date();
+// }
+
+// function closed(){
+//     let x = document.getElementById("para1output");
+//     x.style.display="none";
+// }
+
+// function opened(){
+//     var x = document.getElementById("para1output");
+//     x.style.display="block";
+// }
+
+/*---------------------------------------------------------*/
+
+alert("Welcome to my web");
